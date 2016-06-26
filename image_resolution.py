fd66207894b8e2420d836bb62f892f827f5faca7
Alex Qiu <subactaq@gmail.com>
2016-06-26 10:24:12 -0400

    fixed style conventions

--- image_resolution.py
+++ image_resolution.py
@@ -1,15 +1,18 @@
 """
 Resizes an image to the selected paramters
 Scales Ratio
-For parameters that keep the same ratio effectively 
-performs compression 
+For parameters that keep the same ratio effectively
+performs compression
 """
-# Imports 
-import numpy as np
+
 import cv2
-# Resizes image according to selected parameters
-# if image contains the same x,y dimensions then 
-# the same image is returned 
-def resize(img,x_dim,y_dim):
-	img_resized = cv2.resize(img,(x_dim, y_dim), interpolation = cv2.INTER_CUBIC)
-	return img_resized 
+
+_DEFAULT_WIDTH = 720
+_DEFAULT_HEIGHT = 1280
+
+def resize(image, width=_DEFAULT_WIDTH, height=_DEFAULT_HEIGHT):
+    """
+	Resizes image to given dimensions, width, height, which should be positive integers
+    """
+    image_resized = cv2.resize(image, (width, height), interpolation=cv2.INTER_CUBIC)
+    return image_resized
